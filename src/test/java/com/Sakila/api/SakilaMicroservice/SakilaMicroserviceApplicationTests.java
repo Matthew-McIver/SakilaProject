2fa778970ce3eca85f0fc6831f1b14f69fceb422
Matthew <matthewpetermciver@gmail.com>
2022-10-20 12:00:02 +0100

    Added project

---
+++ src/test/java/com/Sakila/api/SakilaMicroservice/SakilaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sakila.api.SakilaMicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SakilaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
