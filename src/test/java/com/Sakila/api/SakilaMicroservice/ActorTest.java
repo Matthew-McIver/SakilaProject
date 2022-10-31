c8e68d9d4161938f995a8c53481d19728d85a640
Matthew <matthewpetermciver@gmail.com>
2022-10-31 16:52:53 +0200

    100% actor coverage

--- src/test/java/com/Sakila/api/SakilaMicroservice/ActorTest.java
+++ src/test/java/com/Sakila/api/SakilaMicroservice/ActorTest.java
@@ -7,8 +7,20 @@ public class ActorTest {
     Actor testActor = new Actor();
 
     @Test
-    public void testSetActorFirstName(){
-        testActor.setFirstName("ACADEMY DINOSAUR");
-        Assertions.assertEquals("ACADEMY DINOSAUR", testActor.getFirstName(), "Setter for first name is incorrect");
+    public void testActorId(){
+        testActor.setActorId(1);
+        Assertions.assertEquals(1, testActor.getActorId(), "Setter for actor ID is incorrect.");
+    }
+
+    @Test
+    public void testActorFirstName(){
+        testActor.setFirstName("Bazza");
+        Assertions.assertEquals("Bazza", testActor.getFirstName(), "Setter for first names is incorrect.");
+    }
+
+    @Test
+    public void testActorLastName(){
+        testActor.setLastName("Gracie");
+        Assertions.assertEquals("Gracie", testActor.getLastName(), "Setter for last names is incorrect.");
     }
 }
