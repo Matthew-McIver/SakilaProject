cb08b118c25fa08eda952c1dcb27386c5008c629
Matthew <matthewpetermciver@gmail.com>
2022-11-14 14:16:26 +0200

    removing public modifier test

--- src/test/java/sakila/api/ActorModelTest.java
+++ src/test/java/sakila/api/ActorModelTest.java
@@ -3,11 +3,11 @@ package sakila.api;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class ActorModelTest {
+class ActorModelTest {
     ActorModel testActor = new ActorModel("Matthew", "McIver");
     @Test
-    public void testActorFirstName(){
+    void testActorFirstName(){
         Assertions.assertEquals("Matthew", testActor.getFirstName(), "Getter for first names is incorrect."); }
     @Test
-    public void testActorLastName(){
+    void testActorLastName(){
         Assertions.assertEquals("McIver", testActor.getLastName(), "Getter for last names is incorrect."); } }
